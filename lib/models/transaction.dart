0bab2264ffed46ed4110146a96158d4426029214
CSIRamone <cidinei_inacio@hotmail.com>
2023-07-07 01:31:02 -0300

    layout mainAxis&crossAxis class Transaction

---
+++ lib/models/transaction.dart
@@ -0,0 +1,13 @@
+class Transaction {
+  late final String id;
+  late final String title;
+  late final double value;
+  late final DateTime date;
+
+  Transaction({
+    required id,
+    required title,
+    required value,
+    required date,
+  });
+}
